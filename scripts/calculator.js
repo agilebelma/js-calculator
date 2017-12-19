3dd8983c98ec75d0675cb32d24d697b4be7e7549
AgileBelma <agilebelma@gmail.com>
2017-12-19 14:22:02 +1300

    capture buttons' click and do calculation

--- scripts/calculator.js
+++ scripts/calculator.js
@@ -6,14 +6,128 @@ function startApp() {
 
     let allButtons = document.getElementsByClassName("calButton");
 
-    for (let i=0; i<allButtons.length; i+=1) {
+    for (let i = 0; i < allButtons.length; i += 1) {
         let button = allButtons[i];
         button.addEventListener("click", buttonClicked);
     }
 
-
 }
+
+var numStr = "";
+var arrInput = [];
+
 
 function buttonClicked(evt) {
-    console.log(evt.target.value);
+    let value = evt.target.value;
+
+    switch (value) {
+        case "AC":
+            buttonAllClearPressed();
+            break;
+        case "CE":
+            buttonClearEntryPressed();
+            break;
+        case "%":
+            buttonPercentPressed();
+            break;
+        case ".":
+            buttonDotPressed();
+            break;
+        case "1":
+        case "2":
+        case "3":
+        case "4":
+        case "5":
+        case "6":
+        case "7":
+        case "8":
+        case "9":
+        case "0":
+            buttonNumPressed(value);
+            break;
+        case "+":
+        case "-":
+        case "*":
+        case "/":
+        case "=":
+            buttonOprPressed(value);
+            break;
+        default:
+            console.log("Something wrong!! value = ", value);
+
+    }
 }
+
+function display(value) {
+
+    let displayPanel = document.getElementById("displayPanel");
+
+    displayPanel.innerHTML = value;
+}
+
+
+function buttonAllClearPressed() {
+    buttonClearEntryPressed();
+    arrInput = [];
+}
+
+function buttonClearEntryPressed() {
+    numStr = "";
+    display("0");
+}
+
+function buttonPercentPressed() {
+    console.log("Percent Pressed");
+}
+
+function buttonDotPressed() {
+    console.log("dot pressed");
+}
+
+function buttonNumPressed(num) {
+    console.log("num pressed: ", num);
+
+    if (numStr.length < 17) {
+        numStr += num;
+    }
+
+    display(numStr);
+}
+
+function buttonOprPressed(opr) {
+    console.log("operator pressed", opr);
+    console.log("numStr :", numStr);
+
+    if (numStr.length <= 0) {
+        let displayPanel = document.getElementById("displayPanel");
+        numStr = displayPanel.innerText;
+    }
+
+    arrInput.push(numStr);
+    arrInput.push(opr);
+
+    let statementStr = arrInput.join(" ");
+    numStr = "";
+    console.log("statement: ", statementStr);
+
+    calculate(arrInput);
+
+}
+
+
+function calculate(arrInput) {
+    let arrLength = arrInput.length;
+
+    let statementStr = arrInput.slice(0, arrLength-1).join(" "); 
+    console.log(statementStr);
+
+    let result = eval(statementStr);
+    display(result);
+    console.log("result: ", result);
+
+    if (arrInput[0] == "=") {
+        arrInput = [];
+    }
+
+}
+
